52cc4ba6438156e6bfd29017648a05323473ceab
unknown <theusjt49@gmail.com>
2020-03-17 16:22:29 -0500

    impliment roles within my application and now seeding roles in database

---
+++ Project CapStone Mentorship Service/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Project_CapStone_Mentorship_Service.Data;
+
+[assembly: HostingStartup(typeof(Project_CapStone_Mentorship_Service.Areas.Identity.IdentityHostingStartup))]
+namespace Project_CapStone_Mentorship_Service.Areas.Identity
+{
+   
+    public class IdentityHostingStartup : IHostingStartup
+    {
+    //    app.UseRouting();
+    //    app.UseStaticFiles();
+    //    app.UseAuthentication();
+    //    app.UseEndpoints(endpoints =>
+    //{
+    //    endpoints.MapControllers();
+    //    endpoints.MapRazorPages();
+    //});
+    //    services.AddMvc();
+
+        public void Configure(IWebHostBuilder builder)
+        {
+
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- Project CapStone Mentorship Service/Data/ApplicationDbContext.cs
+++ Project CapStone Mentorship Service/Data/ApplicationDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Project_CapStone_Mentorship_Service.Models;
 
 namespace Project_CapStone_Mentorship_Service.Data
 {
@@ -12,5 +14,18 @@ namespace Project_CapStone_Mentorship_Service.Data
             : base(options)
         {
         }
+
+        public DbSet<Student> Students{ get; set; }
+        public DbSet<Mentor> Mentors { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().HasData(new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" });
+            builder.Entity<IdentityRole>().HasData(new IdentityRole { Name = "Student", NormalizedName = "STUDENT" });
+            builder.Entity<IdentityRole>().HasData(new IdentityRole { Name = "Mentor", NormalizedName = "MENTOR" });
+        }
     }
+
 }
