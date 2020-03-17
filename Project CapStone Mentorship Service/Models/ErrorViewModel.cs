dd39e23c10ef3b425c61b3561c664cf436e6b40e
unknown <theusjt49@gmail.com>
2020-03-17 15:11:15 -0500

    init

---
+++ Project CapStone Mentorship Service/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project_CapStone_Mentorship_Service.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
