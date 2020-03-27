00d6f4ac54a5cfcb86dde6c243e463981100caef
unknown <theusjt49@gmail.com>
2020-03-27 11:06:44 -0500

    In home controller refactored index method so that after login the mentor or student will be sent to mentor or student index

--- Project CapStone Mentorship Service/Services/EmailServices.cs
+++ Project CapStone Mentorship Service/Services/EmailServices.cs
@@ -10,7 +10,41 @@ namespace Project_CapStone_Mentorship_Service.Services
 {
     public class EmailServices
     {
-      
+        private static void Main()
+        {
+            Execute().Wait();
+        }
 
+        static async Task Execute()
+        {
+            var mentorEmail = new Mentor();
+            var studentEmails = new Student();
+            var forms = new LessonActivity();
+            var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(mentorEmail.Email, "Mentor Email");
+            var subject = forms.Description;
+            var to = new EmailAddress(studentEmails.Parent_Email, "Parent Email");
+            var plainTextContent = "This is an automated email";
+            var htmlContent = "<strong>automated email</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
+
+        static async Task Execute2()
+        {
+            var mentorEmail = new Mentor();
+            var studentEmails = new Student();
+            var forms = new LessonActivity();
+            var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(mentorEmail.Email, "Mentor Email");
+            var subject = forms.Description;
+            var to = new EmailAddress(studentEmails.Instructor_Email, "Parent Email");
+            var plainTextContent = "This is an automated email";
+            var htmlContent = "<strong>automated email</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
     }
 }
